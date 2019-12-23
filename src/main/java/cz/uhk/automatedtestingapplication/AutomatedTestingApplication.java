1615bb4bd59f90dc523720fa90086bb9158f2b75
Tomáš <kudrnatomas000@gmail.com>
2019-12-23 21:36:30 +0100

    Initial commit

---
+++ src/main/java/cz/uhk/automatedtestingapplication/AutomatedTestingApplication.java
@@ -0,0 +1,13 @@
+package cz.uhk.automatedtestingapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatedTestingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomatedTestingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/uhk/automatedtestingapplication/AutomatedTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.uhk.automatedtestingapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomatedTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
