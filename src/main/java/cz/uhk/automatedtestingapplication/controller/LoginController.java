d3f6b25b6c86f7477e0d4d0b97e4c602e6ed3bdc
Tomáš <kudrnatomas000@gmail.com>
2020-08-03 06:41:32 +0200

    AsyncConfig deleted, RolesFactory deleted.

--- src/main/java/cz/uhk/automatedtestingapplication/controller/LoginController.java
+++ src/main/java/cz/uhk/automatedtestingapplication/controller/LoginController.java
@@ -5,21 +5,15 @@ import cz.uhk.automatedtestingapplication.dao.UserDao;
 import cz.uhk.automatedtestingapplication.model.Role;
 import cz.uhk.automatedtestingapplication.model.User;
 import cz.uhk.automatedtestingapplication.service.FileSystemManagementService;
-import cz.uhk.automatedtestingapplication.service.RolesFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 @Controller
